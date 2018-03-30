0013cfed10376253ad8700f4734e3c7546a6b79a
vikas <vikas.jain8596@gmail.com>
2018-03-30 19:43:27 +0530

    first commit

---
+++ csvfile.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    float x, y, z;
+    int i;
+    srand(getpid());
+
+
+    FILE *fp;
+    fp = fopen("file1.txt","w");
+    fprintf(fp,"S.no.,x,y,z\n");
+    for(i = 0; i < 100; i++)
+    {
+        x = (rand() % 100) / 2.5868f;
+        y = (rand() % 100) / 2.5868f;
+        z = x*y;
+        fprintf(fp,"%d, %f, %f, %.10f\n", i+1, x, y, z);
+    }
+    printf("\nFILE SAVED!");
+    fclose(fp);
+    return 0;
+}
